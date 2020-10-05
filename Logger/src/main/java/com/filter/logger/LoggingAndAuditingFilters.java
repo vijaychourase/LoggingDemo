7a7a292afd8e1ef19109320d0d196d83e3e4ab29
vijaychourase <71980716+vijaychourase@users.noreply.github.com>
2020-10-05 22:01:10 +0530

    Update LoggingAndAuditingFilters.java

--- Logger/src/main/java/com/filter/logger/LoggingAndAuditingFilters.java
+++ Logger/src/main/java/com/filter/logger/LoggingAndAuditingFilters.java
@@ -50,6 +50,7 @@ public class LoggingAndAuditingFilters implements Filter {
 		filterConfig.getServletContext().log("Updated logger filter");
 		filterConfig.getServletContext().log("more updateds");
 		filterConfig.getServletContext().log("more more updateds");
+		//new comments line
 
 	}
 
